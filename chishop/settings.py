46f9c8a64e68fe94ecc0f13262402baa264a1d13
Samuel Sutch <sam@altertap.com>
2011-05-03 15:24:49 -0700

    fixed media uploading config, see http://ssutch.org/chishop-epio

--- chishop/settings.py
+++ chishop/settings.py
@@ -21,13 +21,6 @@ SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 MANAGERS = ADMINS
 
-#DATABASE_ENGINE = ''
-#DATABASE_NAME = ''
-#DATABASE_USER = ''
-#DATABASE_PASSWORD = ''
-#DATABASE_HOST = ''
-#DATABASE_PORT = ''
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -48,7 +41,7 @@ USE_I18N = True
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 #MEDIA_ROOT = os.path.join(os.path.abspath(os.path.dirname(chishop.__file__)), 'media')
-MEDIA_ROOT = '../data'
+MEDIA_ROOT = '../data/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -109,24 +102,12 @@ INSTALLED_APPS = (
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-LOCAL_DEVELOPMENT = True
-
-if LOCAL_DEVELOPMENT:
-    import sys
-    sys.path.append(os.path.dirname(__file__))
 
 ADMINS = (
      ('chishop', 'example@example.com'),
 )
 
 MANAGERS = ADMINS
-
-# DATABASE_ENGINE = 'postgresql_psycopg2'
-# DATABASE_NAME = 'chishop'
-# DATABASE_USER = 'chishop'
-# DATABASE_PASSWORD = 'chishop'
-# DATABASE_HOST = ''
-# DATABASE_PORT = ''
 
 HAYSTACK_SEARCH_ENGINE = 'whoosh'
 HAYSTACK_SITECONF = 'chishop.search_sites'
